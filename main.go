454c0fbb4474ff71f2b20f67603b82633294d5df
dovban <dovban@mdis.ru>
2024-08-27 10:41:10 +0300

    добавлен сервер

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"html/template"
+	"log"
+	"net/http"
 )
 
 type Rsvp struct {
@@ -11,13 +13,54 @@ type Rsvp struct {
 	WillAttend bool
 }
 
+type formData struct {
+	*Rsvp
+	Errors []string
+}
+
 var responses = make([]*Rsvp, 0, 10)
 var templates = make(map[string]*template.Template, 3)
 
 func main() {
 	loadTemplates()
+
+	http.HandleFunc("/", welcomHandler)
+	http.HandleFunc("/list", listHandler)
+	http.HandleFunc("/form", formHandler)
+
+	log.Println("http://localhost:5000")
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
 }
 
 func loadTemplates() {
-	// TODO - load templates here
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for i, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err != nil {
+			log.Panicln(err.Error())
+		} else {
+			templates[name] = t
+			log.Println("Loaded template", i, name)
+		}
+	}
+}
+
+func welcomHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, nil)
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		templates["form"].Execute(writer, formData{
+			Rsvp:   &Rsvp{},
+			Errors: []string{},
+		})
+	}
 }
